1b79132a9b06190f4f6b58911b7c3b92f3591840
sygxde <js1flor9404@hotmail.com>
2024-05-10 05:12:32 +0700

    feat project

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-function isAnagram(s, t) {
-  if (s.length !== t.length) return false;
-  const count = Array(26).fill(0);
-  for (let i = 0; i < s.length; i++) {
-    count[s.charCodeAt(i) - 97]++;
-    count[t.charCodeAt(i) - 97]--;
+function isPrime(n) {
+  if (n <= 1) return false;
+  if (n <= 3) return true;
+  if (n % 2 === 0 || n % 3 === 0) return false;
+  let i = 5;
+  while (i * i <= n) {
+    if (n % i === 0 || n % (i + 2) === 0) return false;
+    i += 6;
   }
-  return count.every((c) => c === 0);
+  return true;
 }
