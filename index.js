09d49dbeb8d3a5c25c257859e678bbf6b8d5ba7f
sygxde <js1flor9404@hotmail.com>
2024-05-14 07:06:30 +0700

    update sdk project commit

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const lcm = (a, b) => {
-  return (a * b) / gcd(a, b);
-};
+function rob(nums) {
+  if (nums.length === 1) return nums[0];
+  const robRange = (start, end) => {
+    let prevMax = 0;
+    let currMax = 0;
+    for (let i = start; i <= end; i++) {
+      const temp = currMax;
+      currMax = Math.max(currMax, prevMax + nums[i]);
+      prevMax = temp;
+    }
+    return currMax;
+  };
+  return Math.max(robRange(0, nums.length - 2), robRange(1, nums.length - 1));
+}
